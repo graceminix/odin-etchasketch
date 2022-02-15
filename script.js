dd853b0f78323cd5f547a9f001b606135a3be9a2
graceminix <graceminix@gmail.com>
2022-02-15 17:34:30 +0100

    Works for 16x16.

--- script.js
+++ script.js
@@ -12,6 +12,33 @@ for (i = 0; i < 16; i++) {
 let box = document.getElementById('grid')
 
 box.addEventListener("mouseover", function( event ) {
-    event.target.style.background = "purple";
+    event.target.className = "etch";
 }, false);
 
+const button = document.querySelector('#heh');
+button.addEventListener("click", reSet);
+
+function reSet() {
+    const color = document.getElementsByClassName('etch')
+    var result = [].slice.call(color);
+    for (i = 0; i < result.length; ++i) {
+        result[i].className = 'square';
+    }
+    var newgrid = 101;
+    while (newgrid  > 100 || newgrid < 0) {
+        newgrid = window.prompt("Please enter a grid size between 0 and 100!");
+    }
+    removeAllChildNodes(container);
+    makeGrid(newgrid);
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function makeGrid(number) {
+    var num = Number(number);
+    
+}
